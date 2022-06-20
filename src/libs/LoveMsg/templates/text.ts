af8999d716a0a708c75d14e6cf3ba287295107d7
zbwwwwww <40452731+zbwwwwww@users.noreply.github.com>
2022-06-20 10:26:22 +0800

    Update text.ts

--- src/libs/LoveMsg/templates/text.ts
+++ src/libs/LoveMsg/templates/text.ts
@@ -52,11 +52,11 @@ ${sayLove.content}\n`
 『网易云音乐热评』${netEaseCloud.content}——${netEaseCloud.source}\n`
   }
 
-  // 添加一句一言
-  if (oneWord) {
-    text += `
-『一言』${oneWord.hitokoto}\n`
-  }
+//   // 添加一句一言
+//   if (oneWord) {
+//     text += `
+// 『一言』${oneWord.hitokoto}\n`
+//   }
 
   // 每日英语
   if (dayEnglish) {
