40f3abd47252eab86db6df305131644ae9b7b446
zbwwwwww <40452731+zbwwwwww@users.noreply.github.com>
2022-06-21 09:30:09 -0500

    Update drinkWater.ts

--- src/libs/LoveMsg/drinkWater.ts
+++ src/libs/LoveMsg/drinkWater.ts
@@ -3,7 +3,6 @@
  * @description 提醒喝水
  */
 import API from '../../api/loveMsg'
-import { getConfig } from '../../utils/getConfig'
 import { wxNotify } from '../WxNotify'
 import { textTemplate } from './templates/text'
 import { textCardTemplate } from './templates/textcard'
@@ -27,39 +26,19 @@ const goodWord = async () => {
 周末快乐！！！😆今天的懒觉睡的还舒服咩~😝🤣今天是${week}，起床后记得喝一大杯水噢~😝，等一会会巴库就来陪你了哦\n`
       } else {
          text += `工作辛苦啦，繁忙之余不要忘记喝水水哦~😆\n`
+         if (sayLove) {
+           text += `今日份土味情话请查收😘:\n
+              ${sayLove.content}\n`
+         }
       }
-      
-     if (sayLove) {
-      text += `
-   ${sayLove.content}\n`
-     }
     console.log('drinkWater', text)
-
     wxNotify(template)
   } catch (error) {
     console.log('goodWord:err', error)
   }
 }
 
-// 天气信息
-const weatherInfo = async () => {
-  try {
-    const weather = await API.getWeather(CONFIG.city_name)
-    if (weather) {
-      const lunarInfo = await API.getLunarDate(weather.date)
-      const template = textCardTemplate({ ...weather, lunarInfo })
-      console.log('weatherInfo', template)
-
-      // 发送消息
-      await wxNotify(template)
-    }
-  } catch (error) {
-    console.log('weatherInfo:err', error)
-  }
-}
-
-// goodMorning
-export const goodMorning = async () => {
-  await weatherInfo()
+// drinkWater
+export const drinkWater = async () => {
   await goodWord()
 }
