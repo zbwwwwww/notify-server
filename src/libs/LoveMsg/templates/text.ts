71aae25f10b41648e8407cc81a50cae1d48f8bf5
zbwwwwww <40452731+zbwwwwww@users.noreply.github.com>
2022-06-21 22:02:03 +0800

    修改工作日与周末的话术

--- src/libs/LoveMsg/templates/text.ts
+++ src/libs/LoveMsg/templates/text.ts
@@ -13,14 +13,14 @@ export const textTemplate = (data: TextTemplateProps) => {
   // 工作日/休息日，需要排除节假日
   const week = weekToday()
   if (['星期六', '星期日'].includes(week)) {
-    text += `
-如果我小楠宝贝已经起床啦！巴库向你说早安呦~，记得吃早饭呀😆\n
-嗯哼哼~今天可是${week}哦，上班别迟到了哦~`
-  }
-  else {
-    text += `
+     text += `
 如果我小楠宝贝还没起床呀！巴库就等着小楠宝贝起床给我说早安呦🤣
 嗯哼~，既然今天是${week}，就让你再睡会懒觉~下次可不能啦~😝\n`
+  }
+  else {
+   text += `
+如果我小楠宝贝已经起床啦！巴库向你说早安呦~，记得吃早饭呀😆\n
+嗯哼哼~今天可是${week}哦，上班别迟到了哦~`
   }
 
   // 添加笑话
